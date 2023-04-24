5c0a88af75d652f7438e6bfed52ea0e72a2936d4
Russkin Artem <artemrusskin92@gmail.com>
2023-04-24 22:20:30 +0300

    1 registration done

--- flutter_projects/flutter_test_project_neuroland/neuroland/lib/features/first_page.dart
+++ flutter_projects/flutter_test_project_neuroland/neuroland/lib/features/first_page.dart
@@ -14,8 +14,8 @@ class _SplashScreenState extends State<SplashScreen> {
   @override
   Widget build(BuildContext context) {
     Timer(const Duration(seconds: 2), () {
-      Navigator.push(
-          context, MaterialPageRoute(builder: (context) => Registration()));
+      Navigator.push(context,
+          MaterialPageRoute(builder: (context) => const Registration()));
     });
     return Container(
       alignment: Alignment.center,
--- flutter_projects/flutter_test_project_neuroland/neuroland/lib/features/registration.dart
+++ flutter_projects/flutter_test_project_neuroland/neuroland/lib/features/registration.dart
@@ -25,7 +25,7 @@ class _RegistrationState extends State<Registration> {
           Align(
             alignment: Alignment.topLeft,
             child: Padding(
-              padding: const EdgeInsets.only(bottom: 65.0, left: 10),
+              padding: const EdgeInsets.only(bottom: 20.0, left: 10),
               child: Image.asset(
                 "assets/images/arrow.png",
                 width: 166,
@@ -47,8 +47,8 @@ class _RegistrationState extends State<Registration> {
               padding: const EdgeInsets.only(bottom: 18.0, right: 0, top: 31),
               child: Image.asset(
                 "assets/images/rightArrow.png",
-                width: 166,
-                height: 150,
+                width: 136,
+                height: 124,
               ),
             ),
           ),
@@ -60,13 +60,18 @@ class _RegistrationState extends State<Registration> {
               fontFamily: 'Roboto',
             ),
           ),
-          const Padding(
-            padding:
-                EdgeInsets.only(top: 30.0, bottom: 10, left: 24, right: 24),
+          Padding(
+            padding: const EdgeInsets.only(
+                top: 30.0, bottom: 10, left: 24, right: 24),
             child: TextField(
-              decoration: InputDecoration(
+              controller: myController,
+              decoration: const InputDecoration(
                 border: InputBorder.none,
                 hintText: "Email или телефон",
+                hintStyle: TextStyle(
+                  fontFamily: 'Roboto',
+                  fontSize: 20,
+                ),
                 fillColor: Color(0xF1F1F1F1),
                 filled: true,
               ),
@@ -75,11 +80,15 @@ class _RegistrationState extends State<Registration> {
             ),
           ),
           const Padding(
-            padding: EdgeInsets.only(bottom: 10, left: 24, right: 24),
+            padding: EdgeInsets.only(bottom: 30, left: 24, right: 24),
             child: TextField(
               decoration: InputDecoration(
                 border: InputBorder.none,
                 hintText: "Пароль",
+                hintStyle: TextStyle(
+                  fontFamily: 'Roboto',
+                  fontSize: 20,
+                ),
                 fillColor: Color(0xF1F1F1F1),
                 filled: true,
               ),
@@ -88,6 +97,79 @@ class _RegistrationState extends State<Registration> {
               expands: false,
             ),
           ),
+          Padding(
+            padding: const EdgeInsets.only(bottom: 10, left: 24, right: 24),
+            child: Container(
+              decoration: const BoxDecoration(
+                borderRadius: BorderRadius.all(Radius.circular(10.0)),
+              ),
+              width: 380,
+              height: 60,
+              child: FloatingActionButton(
+                heroTag: "EnterButton",
+                backgroundColor: null,
+                onPressed: () {
+                  showDialog(
+                      context: context,
+                      builder: (context) {
+                        return AlertDialog(
+                          content: Text(myController.text),
+                        );
+                      });
+                },
+                shape: const RoundedRectangleBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(10))),
+                tooltip: '',
+                child: Container(
+                  alignment: Alignment.center,
+                  decoration: const BoxDecoration(
+                    gradient: LinearGradient(colors: [
+                      Color.fromARGB(255, 36, 33, 123),
+                      Color.fromARGB(255, 58, 73, 238),
+                    ]),
+                    borderRadius: BorderRadius.all(Radius.circular(10.0)),
+                  ),
+                  child: const Text(
+                    'Войти',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontFamily: 'Roboto',
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(bottom: 0, left: 24, right: 24),
+            child: Container(
+              decoration: BoxDecoration(
+                borderRadius: const BorderRadius.all(Radius.circular(10)),
+                border: Border.all(
+                  color: Colors.black,
+                ),
+              ),
+              width: 380,
+              height: 60,
+              child: FloatingActionButton.extended(
+                heroTag: "RegisterButton",
+                backgroundColor: Colors.white,
+                label: const Text(
+                  "Рeгистрация",
+                  style: TextStyle(
+                    fontFamily: 'Roboto',
+                    fontSize: 20,
+                    color: Colors.black,
+                  ),
+                ),
+                onPressed: () {},
+                shape: const RoundedRectangleBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(10))),
+              ),
+            ),
+          )
         ],
       ),
     );
